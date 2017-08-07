23d91c254e88d20b6640a234fee28d72bfef1cc5
Adam Senese <senese04@gmail.com>
2017-08-06 19:05:49 -0500

    Connect nav components

--- webpack.config.js
+++ webpack.config.js
@@ -24,8 +24,9 @@ module.exports = {
     alias: {
       Main: 'app/components/Main.jsx',
       Nav: 'app/components/Nav.jsx',
+      Timer: 'app/components/timer.jsx',
+      Countdown: 'app/components/countdown.jsx',
       applicationStyles: 'app/styles/app.scss'
-
 
     },
     extensions: ['', '.js', '.jsx']
